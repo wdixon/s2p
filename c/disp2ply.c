75fe76c5c3441892faa3e702c6a0e3ee83b62406
Walt Dixon <wdixon@users.noreply.github.com>
2019-02-13 18:11:39 -0500

    syntax

--- c/disp2ply.c
+++ c/disp2ply.c
@@ -215,6 +215,7 @@ int main(int c, char *v[])
 
     // outputs
     double p[2], q[2], X[3];
+    err = 0;
 
     // count number of valid pixels, and determine utm zone
     int npoints = 0;
@@ -245,6 +246,11 @@ int main(int c, char *v[])
         double b[2] = {col + dx, row + dy};
         apply_homography(q, hsec_inv, b);
         if(intersect_rays(X, p, q, rpc_ref, rpc_sec)) {
+	    err++;
+            if(err > 100) {
+                fprintf(stderr, "too many non-basis, aborting\n");
+                return 1;
+            }
             continue;
 	}
 
